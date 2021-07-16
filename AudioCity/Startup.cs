3e6d3c45625c50ba7d13fc13dd22e6691435b618
harry oh <harryoh3831@gmail.com>
2021-07-16 18:58:10 +0800

    migrated database, starting homepage

--- AudioCity/Startup.cs
+++ AudioCity/Startup.cs
@@ -23,7 +23,8 @@ namespace AudioCity
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllersWithViews();
+            services.AddControllersWithViews().AddRazorRuntimeCompilation();
+            services.AddRazorPages().AddRazorRuntimeCompilation();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -43,7 +44,7 @@ namespace AudioCity
             app.UseStaticFiles();
 
             app.UseRouting();
-
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
@@ -51,6 +52,7 @@ namespace AudioCity
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
+                endpoints.MapRazorPages();
             });
         }
     }
