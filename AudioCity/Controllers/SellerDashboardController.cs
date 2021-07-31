77c4e32f8679366a553650ef7e9978f247eb9946
TP046716 <TP046716@mail.apu.edu.my>
2021-08-01 00:46:07 +0800

    Make the revenue table same as nav & change pending to ongoing for admin

--- AudioCity/Controllers/SellerDashboardController.cs
+++ AudioCity/Controllers/SellerDashboardController.cs
@@ -86,8 +86,8 @@ namespace AudioCity.Controllers
                 //do your things
                 //partition key = gig id 
                 //row key = order id 
-                List<OrderEntity> CompletedOrders = OrderEntityHelper.GetAllRevenueOrder(UserId);
-                return View(CompletedOrders);
+                List<OrderEntity> RevenueOrders = OrderEntityHelper.GetAllRevenueOrder(UserId);
+                return View(RevenueOrders);
             }
 
             return View();
